38c65a21abfe5fdad9bcaf9abb757dcd8b59abd1
thpa0001 <thpa0001@stud.kea.dk>
2025-10-01 23:58:31 +0200

    dynamo

--- produkt.js
+++ produkt.js
@@ -1,41 +1,40 @@
-console.log("script loaded...");
+console.log("Hej fra product.js");
 
-const produkt = document.querySelector("#produktside");
-const produktinfo = ["football jersey"];
+const id = new URLSearchParams(window.location.search).get("id");
+const productcontainer = document.querySelector(".produktcontainer");
 
-let listItems = produktinfo.map((titel) => `<li>${titel}</li>`).join("");
+getData(`https://kea-alt-del.dk/t7/api/products/${id}`);
 
-produktside.innerHTML = listItems;
-
-console.log("Nyt array", listItems);
-
-function hentData() {
-  console.log("hentData");
-  fetch("produkt.json")
+function getData(url) {
+  fetch(url)
     .then((res) => res.json())
-    .then(visProdukt);
+    .then((data) => showProduct(data));
 }
+function showProduct(product) {
+  console.log("showProduct: ", product);
 
-function visProdukt(data) {
-  console.log("visProdukt");
+  productcontainer.innerHTML += `<article>
+  <div>
+              <a href="javascript:history.back()" class="back-button">⟨ Tilbage</a>
 
-  const altProdukt = data.jersey;
-  let arrayMedJersey = altProdukt.map(
-    (troje) =>
-      `<article class= "product_card" 
-  <h3>${troje.titel}</h3>
-    <p>Price:${troje.price}</p>
-  <p>Gender:${troje.gender}</p>
-    <p>Display name:${troje.productdisplayname}</p>
+          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}" />
+           </div>
+            <div class="information">
+          <h2>${product.productdisplayname}</h2>
+          <h5>${product.brandname} | ${product.usagetype}</h5>
+          <p>${product.price} DKK</p>
+          <p>Læs mere:</p>
+            </div>
+              <div class="køb_boks">
+            <label for="size">Choose a size</label><br>
+            <select>
+                <option>S</option>
+                <option>M</option>
+                <option>L</option>
+                <option>XL</option>
+            </select><br>
 
-    <p>Category:${troje.category}</p>
-
-  </article>
-   `
-  );
-  console.log("arrayMedJersey:", arrayMedJersey);
-
-  produktside.innerHTML = arrayMedJersey.join("");
+            <button id="basket">Add to basket</button>
+        </div>
+      </article>`;
 }
-
-hentData();
--- produktiste.js
+++ produktiste.js
@@ -16,12 +16,25 @@ function showProducts(products) {
   productcontainer.innerHTML = "";
 
   products.forEach((product) => {
-    productcontainer.innerHTML += `<article>
-        <a href="product.html?id=${product.id}">
+    productcontainer.innerHTML += `
+         <article class="card ${product.soldout ? "soldOut" : ""} ${product.discount ? "discount" : ""}" >
+
+    <a href="product.html?id=${product.id}">
+    <div class="imageContainer">  
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}" />
+      <p>SOLD OUT</p>
+    </div>
           <h2>${product.productdisplayname}</h2>
           <h5>${product.brandname} | ${product.usagetype}</h5>
           <p>${product.price} DKK</p>
+           <div class="discounted_container">
+        <p>
+          Now DKK <span>${product.price - (product.price * product.discount) / 100}</span>,-
+        </p>
+        <p>
+          <span>${product.discount}</span> %
+        </p>
+      </div>
           <p>Læs mere:</p>
         </a>
       </article>`;
